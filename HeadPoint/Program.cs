f84ea17181a5f2ccbb144850eb981b00d05ba3d7
emalinovskiy <emalinovskiy@croc.ru>
2022-12-29 13:36:55 +0300

    Add project files.

---
+++ HeadPoint/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//var data = new string[7] { "ток", "рост", "кот", "торс", "Кто", "фывап", "рок" };
+
+IEnumerable<IEnumerable<string>> Method(string[] data) =>
+    data.GroupBy(str => string.Concat(
+                str.ToLower()
+                    .OrderBy(ch => ch)
+            )
+            .GetHashCode())
+        .Select(gr =>
+            gr.Select(elem => elem)
+        );
